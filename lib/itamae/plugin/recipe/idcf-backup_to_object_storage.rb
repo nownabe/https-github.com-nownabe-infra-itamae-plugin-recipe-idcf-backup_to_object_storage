7287b6e93e5b18f29b197684b175df46ab6a2e1f
nownabe <nownabe@gmail.com>
2015-05-18 09:38:22 +0900

    fix bug

--- lib/itamae/plugin/recipe/idcf-backup_to_object_storage.rb
+++ lib/itamae/plugin/recipe/idcf-backup_to_object_storage.rb
@@ -2,10 +2,19 @@ package "epel-release"
 
 package "s3cmd"
 directory "/etc/s3cmd/idcf"
-template "/etc/s3cmd/idcf/backup_to_object_storage.cfg"
+template "/etc/s3cmd/idcf/backup_to_object_storage.cfg" do
+  owner "root"
+  group "root"
+end
 
 directory "/opt/idcf"
 remote_file "/opt/idcf/backup_to_object_storage.sh" do
+  owner "root"
+  group "root"
   mode "0755"
 end
-template "/etc/cron.d/idcf-backup_to_object_storage"
+
+template "/etc/cron.d/idcf-backup_to_object_storage" do
+  owner "root"
+  group "root"
+end
--- spec/integration/recipe_spec.rb
+++ spec/integration/recipe_spec.rb
@@ -2,6 +2,8 @@ require "spec_helper"
 
 describe file("/etc/cron.d/idcf-backup_to_object_storage") do
   it { should be_file }
+  it { should be_owned_by "root" }
+  it { should be_grouped_into "root" }
   it { should contain "1 * * * * root /bin/bash /opt/idcf/backup_to_object_storage.sh PATH1 BUCKET1 7" }
   it { should contain "1 * * * * root /bin/bash /opt/idcf/backup_to_object_storage.sh PATH2 BUCKET2 7 'COMMAND'" }
 end
@@ -9,10 +11,14 @@ end
 describe file("/etc/s3cmd/idcf/backup_to_object_storage.cfg") do
   it { should be_file }
   it { should contain "access_key = SPEC_ACCESS_KEY" }
+  it { should contain "host_base = ds.jp-east.idcfcloud.com" }
+  it { should contain "host_bucket = %(bucket)s.ds.jp-east.idcfcloud.com" }
   it { should contain "secret_key = SPEC_SECRET_KEY" }
   it { should contain "signature_v2 = True" }
 end
 
 describe file("/opt/idcf/backup_to_object_storage.sh") do
   it { should be_file }
+  it { should be_owned_by "root" }
+  it { should be_grouped_into "root" }
 end
